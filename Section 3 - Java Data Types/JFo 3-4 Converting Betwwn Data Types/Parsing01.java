baa6b82cc99c0a5ae26be998e5a2c8a4a2746059
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2022-09-23 09:36:29 +0700

    exercise 3 part 1

--- Section 3 - Java Data Types/JFo 3-4 Converting Betwwn Data Types/Parsing01.java
+++ Section 3 - Java Data Types/JFo 3-4 Converting Betwwn Data Types/Parsing01.java
@@ -1,18 +1,16 @@
 public class Parsing01 {
-    public static void main(String[] args) {
-        //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
-        
-        
-        
-        
-        //Parse shirtPrice and taxRate, and print the total tax
-        
-        
-        
-        //Try to parse taxRate as an int
-        
-        //Try to parse gibberish as an int
-        
-    }
-    
+  public static void main(String[] args) {
+    // Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
+    String shirtPrice = "15";
+    String taxRate = "0.05";
+    String gibberish = "887ds7nds87dsfs";
+
+    // Parse shirtPrice and taxRate, and print the total tax
+
+    // Try to parse taxRate as an int
+
+    // Try to parse gibberish as an int
+
+  }
+
 }
