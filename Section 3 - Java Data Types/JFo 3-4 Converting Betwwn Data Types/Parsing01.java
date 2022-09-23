d29dc90f0996a095e43ecb70edab477ee04c518c
Muhammad Zahid Masruri <zahid@hellonimbly.com>
2022-09-23 09:39:27 +0700

    done exercise 3 part 2

--- Section 3 - Java Data Types/JFo 3-4 Converting Betwwn Data Types/Parsing01.java
+++ Section 3 - Java Data Types/JFo 3-4 Converting Betwwn Data Types/Parsing01.java
@@ -6,11 +6,13 @@ public class Parsing01 {
     String gibberish = "887ds7nds87dsfs";
 
     // Parse shirtPrice and taxRate, and print the total tax
+    System.out.println(Integer.parseInt(shirtPrice) * Double.parseDouble(taxRate));
 
     // Try to parse taxRate as an int
+    System.out.println(Integer.parseInt(taxRate));
 
     // Try to parse gibberish as an int
-
+    System.out.println(Integer.parseInt(gibberish));
   }
 
 }
